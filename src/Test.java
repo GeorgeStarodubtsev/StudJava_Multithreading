ef664933eee5a4f735cfb26f098994e74760e39f
GeorgeStarodubtsev <geostar88@gmail.com>
2021-10-05 20:53:39 +0300

    3
    add New MyThred

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,22 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("Hello");
+        MyThread thred = new MyThread();
+        thred.start();
 
 
+    }
+}
+
+class MyThread extends Thread {
+    public void run(){
+        while (true){
+            System.out.println("World!");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
